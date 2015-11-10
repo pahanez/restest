09469a61ed5bf935e4ea7fd620e3cae3f3675c05
Pavel Indziukou <pahanez@gmail.com>
2015-11-11 01:35:46 +0300

    init2

--- Restest/app/src/main/java/com/pahanez/restest/MainActivityFragment.java
+++ Restest/app/src/main/java/com/pahanez/restest/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package com.pahanez.restest;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,5 +18,8 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    private void coordinate() {
     }
 }
