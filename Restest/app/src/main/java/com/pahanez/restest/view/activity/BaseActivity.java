8212b4feb5ab59db6d9f08682371cbcfba85ca0b
Pavel Indziukou <pahanez@gmail.com>
2015-12-14 23:37:44 +0300

    ...

--- Restest/app/src/main/java/com/pahanez/restest/view/activity/BaseActivity.java
+++ Restest/app/src/main/java/com/pahanez/restest/view/activity/BaseActivity.java
@@ -1,16 +1,19 @@
 package com.pahanez.restest.view.activity;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 
 import com.pahanez.restest.RestestApplication;
 import com.pahanez.restest.di.component.ApplicationComponent;
+import com.pahanez.restest.di.module.ActivityModule;
 
 /**
  * Created by pindziukou on 13/12/15.
  */
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends Activity {
 
 
     @Override
@@ -26,5 +29,38 @@ public class BaseActivity extends AppCompatActivity {
      */
     protected ApplicationComponent getApplicationComponent() {
         return ((RestestApplication)getApplication()).getApplicationComponent();
+    }
+
+    /**
+     * Get an Activity module for dependency injection.
+     *
+     * @return {@link ActivityModule}
+     */
+    protected ActivityModule getActivityModule() {
+        return new ActivityModule(this);
+    }
+
+    /**
+     * Adds a {@link Fragment} to this activity's layout.
+     *
+     * @param containerViewId The container view to where add the fragment.
+     * @param fragment The fragment to be added.
+     */
+    protected void addFragment(int containerViewId, Fragment fragment) {
+        FragmentTransaction fragmentTransaction = this.getFragmentManager().beginTransaction();
+        fragmentTransaction.add(containerViewId, fragment);
+        fragmentTransaction.commit();
+    }
+
+    /**
+     * Replace a {@link Fragment} to this activity's layout.
+     *
+     * @param containerViewId The container view to where add the fragment.
+     * @param fragment The fragment to be added.
+     */
+    protected void replaceFragment(int containerViewId, Fragment fragment) {
+        FragmentTransaction fragmentTransaction = this.getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(containerViewId, fragment);
+        fragmentTransaction.commit();
     }
 }
